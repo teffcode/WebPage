abcabed9a36e142aa59e5a764cf2273bb1d6064c
teffcode <tefy.1@live.com>
2017-06-29 15:08:12 -0500

    First part of WebPage

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btn_menu = document.getElementById('btn_menu');
+var nav = document.getElementById('nav');
+
+btn_menu.addEventListener('click', function(){
+  nav.classList.toggle('mostrar');
+})
